1e1babdb989eff2a12cbeccd7dff3c704f907a50
Yayati7 <singhpsby@gmail.com>
2026-02-08 00:38:56 +0530

    Fix production API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://minisocial-backend-hj8i.onrender.com",
+  baseURL: "https://minisocial-backend-hj8i.onrender.com/api",
 });
 
 /* ---------- AUTH ---------- */
