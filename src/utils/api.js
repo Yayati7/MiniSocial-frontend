09642a32ee15bb72d64b5123bc4237bce2b93154
Yayati7 <singhpsby@gmail.com>
2026-02-08 00:24:19 +0530

    Fix production API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API,
+  baseURL: "https://mini-social-frontend-three.vercel.app/",
 });
 
 /* ---------- AUTH ---------- */
